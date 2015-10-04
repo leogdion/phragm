58c5d735abbca11bffc750187ed275f8b19815f2
Leo Dion <leogdion@brightdigit.com>
2015-10-04 01:44:49 -0400

    working on listening to new sites

--- research/01-apigit/index.js
+++ research/01-apigit/index.js
@@ -5,42 +5,63 @@ var sequelize = new Sequelize('database', 'username', 'password', {
   dialect: 'sqlite'
 });
 
+
 var uuid = require('node-uuid');
 
 var Site = sequelize.define('Site', {
   siteId: {type : Sequelize.STRING, unique: true}
 });
 
+var Status = sequelize.define('SiteStatus', {
+  name: {type: Sequelize.STRING, unique: true}
+});
+
+Site.belongsTo(Status);
 
 app.get('/', function (req, res) {
-  Site.create({
-    siteId : uuid.v4()
-  }).then(function (site) {
-    res.send(site);
+  Status.findOne({
+    'name' : 'queued'
+  }).then( function (status) {
+    Site.create({
+      siteId : uuid.v4(),
+      status : status
+    }).then(function (site) {
+
+    console.log(site);
+    site.setStatus(status).then(function (site) {
+      res.send(site);
+    });
+    });
   });
 });
 
 sequelize.sync({force: true}).then(function () {
-  
-  var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+  Status.bulkCreate([
+    { name : "queued"},
+    { name : "building"},
+    { name : "ready"}
+  ]).then(function () {
 
-    console.log('Example app listening at http://%s:%s', host, port);
-  });
+    var server = app.listen(3000, function () {
+      var host = server.address().address;
+      var port = server.address().port;
 
-  var CronJob = require('cron').CronJob;
-  var job = new CronJob({
-    cronTime: '*/5 * * * * *',
-    onTick: function() {
-      /*
-       * Runs every weekday (Monday through Friday)
-       * at 11:30:00 AM. It does not run on Saturday
-       * or Sunday.
-       */
-    },
-    start: false
+      console.log('Example app listening at http://%s:%s', host, port);
+    });
+
+    var CronJob = require('cron').CronJob;
+    var job = new CronJob({
+      cronTime: '*/5 * * * * *',
+      onTick: function() {
+        /*
+         * Runs every weekday (Monday through Friday)
+         * at 11:30:00 AM. It does not run on Saturday
+         * or Sunday.
+         */
+      },
+      start: false
+    });
+    job.start();
   });
-  job.start();
 });
 
